a2216a32e92f5cff2968192b91d6f4d21b299512
Ryo-ta8976 <0vr6551b2mb422g@gmail.com>
2020-06-07 20:20:09 +0900

    復元

--- src/registerStore.jsx
+++ src/registerStore.jsx
@@ -1,12 +1,95 @@
-import React from 'react'
-import MenuAppBar from './MenuAppBar'
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CheckBox from "./CheckBox";
+import FormDialog from "./FormDialog";
+import Chip from "@material-ui/core/Chip";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import WorkIcon from "@material-ui/icons/Work";
+import TabPanel from "./TabPanel";
 
-export default class Make_Shift extends React.Component {
-  render() {
-    return (
+const useStyles = makeStyles(() => ({
+  title: {
+    fontSize: 14,
+  },
+  box: {
+    display: "inline",
+    padding: "0.5em 1em",
+    margin: "2em 0",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  h1: {
+    fontFamily: "Courier",
+  },
+  chip: {
+    margin: "10px 50px 10px 50px",
+  },
+  parent: {
+    textAlign: "center",
+  },
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 420,
+    width: 300,
+    overflow: "scroll",
+  },
+  chipGroup: {
+    overflow: "scroll",
+  },
+}));
+
+export default function registerStore() {
+  const classes = useStyles();
+
+  const [personNames, setPersonNames] = useState(["キャッシャー", "ベーカリー", "ウォッシャー"]);
+
+  const handleDelete = (index) => () => {
+    console.info("You clicked the delete icon.");
+    console.log(index);
+    const temp = [...personNames];
+    temp.splice(index, 1);
+    setPersonNames(temp);
+  };
+
+  const [edit, setEdit] = useState(false);
+  const editSkill = edit ? (
+    <CheckBox />
+  ) : (
+    <Grid item xs={12} md={6}>
       <div>
-        <h1>お店設定</h1>
+        <List>
+          {["キャッシャー", "ベーカリー", "ウォッシャー"].map((name, index) => (
+            <ListItem key={index}>
+              <ListItemText primary={name} />
+            </ListItem>
+          ))}
+        </List>
       </div>
-    )
-  }
+    </Grid>
+  );
+
+  const editButton = edit ? (
+    <Button variant="outlined" onClick={() => setEdit(!edit)}>
+      編集終了
+    </Button>
+  ) : (
+    <Button variant="outlined" onClick={() => setEdit(!edit)}>
+      編集
+    </Button>
+  );
+
+  return (
+    <div className={classes.parent}>
+      <div className={classes.box}>
+        <h1 className={classes.h1}>お店登録</h1>
+      </div>
+    </div>
+  );
 }
